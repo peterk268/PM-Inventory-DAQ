8c1027406414a39e053a96db951c5a748f71d7ef
Peter Khouly <45668297+peterk268@users.noreply.github.com>
2023-05-21 10:36:16 -0400

    Initial Commit

---
+++ PM Inventory/PM_InventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PM_InventoryApp.swift
+//  PM Inventory
+//
+//  Created by Peter Khouly on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct PM_InventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
